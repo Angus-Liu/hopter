7f1cc1067e021d8101b81ecfe5d1199902a87382
安光 <1713862733@qq.com>
2018-12-08 21:56:10 +0800

    feat: update DatabaseHelper.

--- src/main/java/org/hopter/framework/helper/DatabaseHelper.java
+++ src/main/java/org/hopter/framework/helper/DatabaseHelper.java
@@ -3,10 +3,14 @@ package org.hopter.framework.helper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.*;
+import org.hopter.framework.util.CollectionUtil;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 数据库操作助手类
@@ -118,4 +122,210 @@ public final class DatabaseHelper {
         }
     }
 
+    /**
+     * 查询实体
+     *
+     * @param entityClass
+     * @param sql
+     * @param params
+     * @param <T>
+     * @return
+     */
+    public static <T> T queryEntity(Class<T> entityClass, String sql, Object... params) {
+        T entity;
+        try {
+            Connection conn = getConnection();
+            entity = QUERY_RUNNER.query(conn, sql, new BeanHandler<>(entityClass), params);
+        } catch (SQLException e) {
+            log.error("query entity failure", e);
+            throw new RuntimeException(e);
+        }
+        return entity;
+    }
+
+    /**
+     * 查询实体列表
+     *
+     * @param entityClass
+     * @param sql
+     * @param params
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> queryEntityList(Class<T> entityClass, String sql, Object... params) {
+        List<T> entityList;
+        try {
+            Connection conn = getConnection();
+            entityList = QUERY_RUNNER.query(conn, sql, new BeanListHandler<>(entityClass), params);
+        } catch (SQLException e) {
+            log.error("query entity list failure", e);
+            throw new RuntimeException(e);
+        }
+        return entityList;
+    }
+
+    /**
+     * 查询并返回单个列值
+     *
+     * @param sql
+     * @param params
+     * @param <T>
+     * @return
+     */
+    public static <T> T query(String sql, Object... params) {
+        T obj;
+        try {
+            Connection conn = getConnection();
+            obj = QUERY_RUNNER.query(conn, sql, new ScalarHandler<>(), params);
+        } catch (SQLException e) {
+            log.error("query failure", e);
+            throw new RuntimeException(e);
+        }
+        return obj;
+    }
+
+    /**
+     * 查询并返回多个列值
+     *
+     * @param sql
+     * @param params
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> queryList(String sql, Object... params) {
+        List<T> list;
+        try {
+            Connection conn = getConnection();
+            list = QUERY_RUNNER.query(conn, sql, new ColumnListHandler<>(), params);
+        } catch (SQLException e) {
+            log.error("query list failure", e);
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
+
+    /**
+     * 查询并返回数组
+     *
+     * @param sql
+     * @param params
+     * @return
+     */
+    public static Object[] queryArray(String sql, Object... params) {
+        Object[] resultArray;
+        try {
+            Connection conn = getConnection();
+            resultArray = QUERY_RUNNER.query(conn, sql, new ArrayHandler(), params);
+        } catch (SQLException e) {
+            log.error("query array failure", e);
+            throw new RuntimeException(e);
+        }
+        return resultArray;
+    }
+
+    /**
+     * 查询并返回数组列表
+     *
+     * @param sql
+     * @param params
+     * @return
+     */
+    public static List<Object[]> queryArrayList(String sql, Object... params) {
+        List<Object[]> resultArrayList;
+        try {
+            Connection conn = getConnection();
+            resultArrayList = QUERY_RUNNER.query(conn, sql, new ArrayListHandler(), params);
+        } catch (SQLException e) {
+            log.error("query array list failure", e);
+            throw new RuntimeException(e);
+        }
+        return resultArrayList;
+    }
+
+    /**
+     * 查询并返回结果集映射（key=列名，value=列值）
+     *
+     * @param sql
+     * @param params
+     * @return
+     */
+    public static Map<String, Object> queryMap(String sql, Object... params) {
+        Map<String, Object> resultMap;
+        try {
+            Connection conn = getConnection();
+            resultMap = QUERY_RUNNER.query(conn, sql, new MapHandler(), params);
+        } catch (SQLException e) {
+            log.error("query map failure", e);
+            throw new RuntimeException(e);
+        }
+        return resultMap;
+    }
+
+    /**
+     * 查询并返回结果映射列表
+     *
+     * @param sql
+     * @param params
+     * @return
+     */
+    public static List<Map<String, Object>> queryMapList(String sql, Object... params) {
+        List<Map<String, Object>> resultMapList;
+        try {
+            Connection conn = getConnection();
+            resultMapList = QUERY_RUNNER.query(conn, sql, new MapListHandler(), params);
+        } catch (SQLException e) {
+            log.error("query map list failure", e);
+            throw new RuntimeException(e);
+        }
+        return resultMapList;
+    }
+
+    /**
+     * 执行更新语句（update、insert、delete）
+     *
+     * @param sql
+     * @param params
+     * @return 影响行数
+     */
+    public static int update(String sql, Object... params) {
+        int rows;
+        try {
+            Connection conn = getConnection();
+            rows = QUERY_RUNNER.update(conn, sql, params);
+        } catch (SQLException e) {
+            log.error("execute update failure", e);
+            throw new RuntimeException(e);
+        }
+        return rows;
+    }
+
+    /**
+     * 插入实体
+     *
+     * @param entityClass
+     * @param fieldMap
+     * @param <T>
+     * @return
+     */
+    public static <T> boolean insertEntity(Class<T> entityClass, Map<String, Object> fieldMap) {
+        if (CollectionUtil.isEmpty(fieldMap)) {
+            log.error("can not insert entity: fieldMap is empty");
+            return false;
+        }
+
+        String sql = "INSERT INTO" + entityClass.getSimpleName();
+        StringBuilder columns = new StringBuilder("(");
+        StringBuilder values = new StringBuilder("(");
+        fieldMap.keySet().forEach(fieldName -> {
+            columns.append(fieldName).append(", ");
+            values.append("?, ");
+        });
+        columns.replace(columns.lastIndexOf(", "), columns.length(), ")");
+        values.replace(values.lastIndexOf(", "), values.length(), ")");
+        sql += columns + " VALUES " + values;
+
+        Object[] params = fieldMap.values().toArray();
+
+        return update(sql, params) == 1;
+    }
 }
